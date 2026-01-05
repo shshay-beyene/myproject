cb530b3646bb50ff9fdc3e9f3468c113682b2574
Shshay Beyene <shshaybeyene702@gmail.com>
2026-01-05 15:16:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+
+    //var greeting = "Hello, World!";
+    document.writeln("Hello, World! sis");
+    function myfunction(){
+        document.getElementById("demo").innerHTML="paragraph changed";
+          }
+
+
+            var Hello = " This is javascript training!";
+            document.writeln(Hello);
+            function MyFunction(){
+            document.getElementById("11").innerHTML= Date();
+            
+
+            }
+            //window.alert("Welcome to javascript programming");
+            function MyFunction(){
+const targetElement = document.getElementById("output-area");
+targetElement.innerHTML = "<h2>Dynamic Content</h2>"; // Renders as an H2 heading
+//targetElement.textContent = "<h2>Plain Text</h2>"; // Displays the raw H2 tags as text
+            }
+             function highlightInput(elm)
+            { 
+               elm.style.background = "yellow"; 
+            } 
+         function losshighlight(elm)
+            { 
+               elm.style.background = "white"; 
+            } 
+
+
+               let now = new Date();
+     let dayOfWeek = now.getDay();  // Sunday - Saturday : 0 - 6
+
+//Or let dayOfWeek = new Date().getDay(); 
+if(dayOfWeek == 5) 
+           {
+                 document.write("Have a nice weekend!");
+           }
